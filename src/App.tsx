e65ae430f1064a6ed3b8ec2c5f8b0c04cc61cd65
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-05 00:19:04 -0700

    Wrapping up for the night
    Should probably get some sleep

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,37 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="cookie.tsx"
-          // target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        */}
         <nav>
-          <ul>
-            <li><Link to="/cookie"></Link></li>
-          </ul>
+          <Link to="/">Home</Link>
         </nav>
-
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <p>
+          <a href="cookie.tsx"></a>
+        </p>
       </header>
-    </div>
+    </Router>
   );
+}
+
+function Home() {
+  return (
+    <h1>
+      Sup
+    </h1>
+  )
 }
 
 export default App;
