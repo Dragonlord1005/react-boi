819a3b45358cf2ed25d3e0740dee91650479bbfa
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-07 17:39:10 +0000

    First commit, I think, added the neccesary packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import React from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import { Outlet, Link } from "react-router-dom"
 
 export default function App() {
   return (
     <div>
-      <h1>Bookkeeper!</h1>
+      <h1>Blog</h1>
       <nav>
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link>
       </nav>
       <Outlet />
     </div>
