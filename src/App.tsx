b2ebaaa23e609166cf9bb61589fc0713c731dcc5
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-08 20:25:48 +0000

    fixed a ton of broken stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import "./App.css"
 import { Outlet, Link } from "react-router-dom"
 import LoginButton from './components/Login';
+import LogoutButton from './components/Logout'
 
 export default function App() {
   return (
     <div>
       <h1>Blog</h1>
       <LoginButton />
+      <LogoutButton />
       <nav>
       </nav>
       <Outlet />
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
+const clientId: string = process.env.REACT_APP_CLIENTID as string;
+const domain: string = process.env.REACT_APP_DOMAIN as string;
+
 
 ReactDOM.render(
   <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri={window.location.origin}
   >
     <App />
   </Auth0Provider>,
