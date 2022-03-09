ef370d32d6d5e5a0c889d77706a8cbc88ee99530
Drago <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-09 16:51:33 -0700

    Update test.ts
    Made it so that it makes sure login and logout buttosns exist

--- cypress/integration/test.ts
+++ cypress/integration/test.ts
@@ -1,5 +1,8 @@
 describe('Join Site', () => {
   it('Visits the site', () => {
     cy.visit('http://localhost:3000/')
+
+    cy.get('div > :nth-child(3)')
+    cy.get('div > :nth-child(3)')
   })
 })
