d6e12dd9e7be7aa957f568cb77d5d6710ff7455a
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-10 19:41:05 +0000

    adding material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import React from "react"
-import "./App.css"
 import { Outlet, Link } from "react-router-dom"
 import LoginButton from './components/Login';
 import LogoutButton from './components/Logout'
+import '@fontsource/roboto/400.css';
+import CssBaseline from '@mui/material/CssBaseline';
 
 export default function App() {
   return (
-    <div>
-      <h1>Blog</h1>
-      <LoginButton />
-      <LogoutButton />
-      <nav>
-      </nav>
-      <Outlet />
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <div>
+        <h1>Blog</h1>
+        <LoginButton />
+        <LogoutButton />
+        <nav>
+        </nav>
+        <Outlet />
+      </div>
+    </React.Fragment>
   )
 }
