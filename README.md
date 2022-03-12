469d2d291a7e807a0cbfb685e98764521e1aadec
Dragonlord1005 <68302450+Dragonlord1005@users.noreply.github.com>
2022-03-11 17:21:50 -0700

    Revert "feat: Blog"

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# react-boi
- 
+# React Boi
+
+A personal project where I use typescript for react, without having any previous knowledge of typescript.
--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 import { Outlet, Link } from "react-router-dom"
-import LoginButton from './components/Login';
-import LogoutButton from './components/Logout'
-import '@fontsource/roboto/400.css';
-import CssBaseline from '@mui/material/CssBaseline';
 
 export default function App() {
   return (
-    <React.Fragment>
-      <CssBaseline />
-      <div>
-        <h1>Blog</h1>
-        <LoginButton />
-        <LogoutButton />
-        <nav>
-        </nav>
-        <Outlet />
-      </div>
-    </React.Fragment>
+    <div>
+      <h1>Bookkeeper!</h1>
+      <nav>
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet />
+    </div>
   )
 }
